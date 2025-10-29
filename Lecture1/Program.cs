9e42b2ab8b96182f48e09cf4d12655202371b1f4
Suleyman Meral <mrlslymn02@gmail.com>
2025-10-29 21:26:46 +0300

    Ders1 bitti

---
+++ Lecture1/Program.cs
@@ -0,0 +1,95 @@
+﻿
+// type variableName = value;
+
+// int string char bool double float decimal long 
+
+//string name = "Süleyman";
+
+//int age = 23;
+
+//char gender = '?';
+
+//double kdv = 0.17;
+
+//Console.WriteLine(name);
+//Console.Write(age);
+//Console.WriteLine(gender);
+//Console.Write(kdv);
+
+// sınav soruus olabilir
+//Console.WriteLine("Merhaba");
+//Console.Write("Merhaba");
+//Console.Write("Dünya");
+//Console.WriteLine("Merhaba");
+//Console.Write("Dünya");
+
+
+//int num;
+
+
+//num = 24;
+
+//string ad;
+
+//ad = "Mukaddes";
+
+//var price = 5.3;  
+
+
+//double myVariable = 6.2;
+
+//myVariable = 9.4;
+
+//Console.WriteLine(myVariable);
+
+// aşağıda verilen çıktıyı konsola yazdıran kodu yazınız. bmw  x5 300 true A  bunlar değişkenlerden gelecek
+// arabanın markası: bmw , modeli: x5, hızı:300, satılık mı: true, kod: A   bmw  x5 300 true A 
+
+
+//string brand = "bmw";
+//string model = "x5";
+//int speed = 300;
+//bool isSale = true;
+//char code = 'A';
+
+//string output = $"Arabanın markası: {brand}, modeli: {model}, hızı: {speed}, satılık mı:{isSale}, kod:{code}";
+
+//Console.WriteLine(output);
+
+
+
+//Console.WriteLine("arabanın markası: "+brand+" modeli: "+model + " hızı: "+speed + " satılık mı: "+isSale+" kod: "+code);
+
+//Console.WriteLine("Arabanın markası: {0} , modeli: {1} , hızı: {2} , satılık mı: {3} , kod: {4}", brand, model, speed, isSale, code);
+
+//Console.WriteLine($"Arabanın markası: {brand}, modeli: {model}, hızı: {speed}, satılık mı:{isSale}, kod:{code}");
+
+// arabanın markası: bmw modeli: x5  hızı: 300 satılık mı: true  kod: A
+
+
+
+
+//// immutable record 
+// const double kdv = 6.2;
+
+//kdv = 9.4;
+
+//Console.WriteLine(kdv);
+
+
+//string name = "Süleyman";
+
+//string surname = "Meral";
+
+//int age = 23;
+
+//string FullName = name + " "+  surname + " " + age;
+
+//Console.WriteLine(FullName);
+
+
+//int age = 20; string name = "S";
+
+//int age = 20, number = 15, weight = 85;
+
+
